9cd919ac0f086d00f4a619b446c28a9100af6182
Afsanab <abhuiyan8983@bths.edu>
2025-06-02 16:45:20 -0400

    Fix: set host and port for Render

--- create_graph.py
+++ create_graph.py
@@ -299,5 +299,6 @@ def update_graph_and_list(clickData, search_value):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
